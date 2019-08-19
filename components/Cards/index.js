7555af6b0ea3446033bffe70f04cb4faf1b5de8e
tevin davis <aod.td1105@gmail.com>
2019-08-18 22:09:39 -0400

    Finished the header and the tab section of the sprint still working on the card section

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function cardCreator(obj) {
+    const cardContainer = document.querySelector('.cards-container');
+    const card = document.createElement('div');
+    div.classList.add('card');
+
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent = obj.headline;
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    const image = document.createElement('img');
+    image.src = 'obj.authorPhoto'
+
+    const name = document.createElement('span');
+    name.textContent = obj.authorName;
+
+    imgContainer.appendChild(image);
+    author.appendChild(imgContainer);
+    author.appendChild(name);
+    card.appendChild(headline);
+    card.appendChild(author);
+
+    return card
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(resolve => {
+        //People is an array of keys from the article object
+        let people = Object.keys(resolve.data.articles)
+        people.forEach(key => {
+            resolve.data.articles[`'${key}'`].forEach(items => {
+                cardCreator(items)
+                cardContainer.appendChild(items)
+            })
+            
+        })
+        // return cardContainer
+        console.log(resolve.data.articles)
+    }).catch(error => {
+        console.error(error)
+    });
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(resolve => {
+        let tab = document.querySelector('.topics')
+        let topic = resolve.data.topics;
+        topic.forEach(listItem => {
+            let item = document.createElement('div')
+            item.classList.add('tab');
+            item.textContent = listItem;
+            tab.appendChild(item);
+        })
+        
+        return tab
+    })
+        .catch(error => {
+        console.error(error)
+    });
+    
